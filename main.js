c561ffed9f908b6d53161187454584e13cccb5d0
Adam Johnson <adam.johnson311@gmail.com>
2019-02-28 15:13:20 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+  $('#bookproject').click(function() {
+    window.location.href = "http://18.222.145.189";
+  })
+  $('#bicycleproject').click(function() {
+    window.location.href = "http://www.msn.com";
+  })
+})
