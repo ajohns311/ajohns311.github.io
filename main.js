3f3df15298a1978fd784ba0059773aef2e87ac04
Adam Johnson <adam.johnson311@gmail.com>
2019-07-02 17:13:49 -0500

    ip fix

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
     window.location.href = "http://18.222.145.189";
   })
   $('#bicycleproject').click(function() {
-    window.location.href = "http://18.219.217.217";
+    window.location.href = "http://18.219.217.127";
   })
   $('#wallproject').click(function() {
     window.location.href = "http://18.220.22.219";
